a589b42126cd4c855a7ea577cf1fb858d0566f05
Phimos <41637559+Phimos@users.noreply.github.com>
2018-09-03 18:34:32 +0800

    2018/9/3

---
+++ POJ3979.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int my_gcd(int a, int b)
+{
+	while (a % b)
+	{
+		a %= b;
+		swap(a, b);
+	}
+	return b;
+}
+int main()
+{
+	string equation;
+	while(cin>>equation)
+	{
+		int a = equation[0] - '0', b = equation[2] - '0', c = equation[4] - '0', d = equation[6] - '0';
+		char op = equation[3];
+		int numerator, denominator;
+		denominator = b * d;
+		if (op == '+')
+			numerator = a * d + c * b;
+		else
+			numerator = a * d - c * b;
+		int gcd = my_gcd(abs(numerator), denominator);
+		if (numerator%denominator == 0)
+			cout << numerator / denominator << endl;
+		else
+			cout << numerator / gcd << '/' << denominator / gcd << endl;
+	}
+	return 0;
+}
